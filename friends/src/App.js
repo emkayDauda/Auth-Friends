beb5f4df9df29a43548ac6c09167378025593746
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-15 13:25:47 +0100

    :sparkles: Complete sign in process

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import Login from "./components/Login";
 import './App.css';
+import axios from "axios";
 
 
 const login = (formValues, actions) => {
-
+  axios.post("http://localhost:5000/api/login", {
+    username: formValues.username,
+    password: formValues.password,
+  })
+  .then(res => {
+    console.log(res.data);
+    localStorage.setItem('token', res.data.payload)
+  })
+  .catch(error => {
+    console.log(error);
+  })
 
   actions.resetForm()
 }
